e6215504a33ea5f66c51ab102722ff1479153def
imarkov <imarkov1275@gmail.com>
2023-10-11 21:30:51 -0500

    Modified AddPharmacy page

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,23 @@ import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar.component";
 import CustomLinearProgress from "./components/custom-linear-progress/CustomLinearProgress.component";
+import Pharmacies from "./pages/pharmacies/Pharmacies.page";
+import Pharmacy from "./pages/pharmacies/Pharmacy.page";
+import AddPharmacy from "./pages/pharmacies/AddPharmacy.page";
 
-const Pharmacies = lazy(() => import("./pages/pharmacies/Pharmacies.page"));
+// const Pharmacies = lazy(() => import("./pages/pharmacies/Pharmacies.page"));
 const Prescriptions = lazy(
   () => import("./pages/prescriptions/Prescriptions.page")
 );
 const Pharmacists = lazy(() => import("./pages/pharmacists/Pharmacists.page"));
-const AddPharmacy = lazy(() => import("./pages/pharmacies/AddPharmacy.page"));
+//const AddPharmacy = lazy(() => import("./pages/pharmacies/AddPharmacy.page"));
 const AddPrescription = lazy(
   () => import("./pages/prescriptions/AddPrescription.page")
 );
 const AddPharmacist = lazy(
   () => import("./pages/pharmacists/AddPharmacist.page")
 );
-const Pharmacy = lazy(() => import("./pages/pharmacies/Pharmacy.page"));
+// const Pharmacy = lazy(() => import("./pages/pharmacies/Pharmacy.page"));
 const Prescription = lazy(
   () => import("./pages/prescriptions/Prescription.page")
 );
